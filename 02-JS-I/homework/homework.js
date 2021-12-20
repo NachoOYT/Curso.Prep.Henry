97827fbafd9ab8d7006232b0989e9ce99f3f3f6e
NachoOYT <nachoclerici97@gmail.com>
2021-12-20 17:12:55 -0300

    primer bool

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'Lucho';
 const nuevoNum = 4;
 
 // Crea una variable booleana:
-const nuevoBool = "verdadero";
+const nuevoBool = "Ayudar a las personas está bien";
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
